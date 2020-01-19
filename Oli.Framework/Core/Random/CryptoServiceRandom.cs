1b915c226789d53f7940c39bc218d47e0dcf3117
m.olfatmiri <m.olfatmiri@parham-co.ir>
2020-01-19 16:06:18 +0330

    Added RandomValueProvider with Tests

---
+++ Oli.Framework/Core/Random/CryptoServiceRandom.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Oli.Framework.Core.Random
+{
+    public sealed class CryptoServiceRandom : Random, IDisposable
+    {
+        private readonly byte[] _buf = new byte[sizeof(uint) * 64];
+        private int _i;
+        private readonly RandomNumberGenerator _rng;
+
+        public CryptoServiceRandom() : this(RandomNumberGenerator.Create())
+        {
+        }
+
+        public CryptoServiceRandom(RandomNumberGenerator rng)
+        {
+            _rng = rng ?? throw new ArgumentNullException(nameof(rng));
+            _i = _buf.Length;
+        }
+
+        public override uint GetNum()
+        {
+            if (_i >= _buf.Length)
+            {
+                _rng.GetBytes(_buf);
+                _i = 0;
+            }
+
+            var result = BitConverter.ToUInt32(_buf, _i);
+            _i += sizeof(uint);
+            return result;
+        }
+
+        public void Dispose()
+        {
+            _rng.Dispose();
+        }
+    }
+}
---
+++ Oli.Framework/Core/Random/Sets.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Oli.Framework.Core.Random
+{
+    public static class Sets
+    {
+        public const string Lower = "abcdefghijklmnopqrstuvwxyz";
+        public const string Upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public const string Digits = "0123456789";
+        public const string Alphanumerics = Lower + Upper + Digits;
+        public const string Symbols = "!@#$%^&*()";
+        public const string FullSymbols = @"!""#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+
+        public static IEnumerable<string> AlphanumericGroups
+        {
+            get { return new[] {Lower, Upper, Digits}; }
+        }
+
+    }
+}
